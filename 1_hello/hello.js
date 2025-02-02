ccb1bf49fc25d8274ede2c289ed6ff758753ca7c
Ompawar7511 <op6651405@gmail.com>
2025-02-02 13:18:27 +0530

    first commit

---
+++ 1_hello/hello.js
@@ -0,0 +1,8 @@
+// printing Method in JavaScript
+console.log("Hello World")
+process.stdout.write("hello world . JS");
+//For printing table
+console.table({ name: "abc", ID: 123 });
+//error handling.
+console.error("Error!");
+console.warn("Warning!");
